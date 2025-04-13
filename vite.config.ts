df13321b73588089bc35ebf3c96c0b57be8ae2ca
Enzo Bomark <71819938+EnzoBomark@users.noreply.github.com>
2025-04-13 15:00:13 +0200

    chore: update base URL in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vanillaExtractPlugin()],
-  base: '/enzo-bomark/',
+  base: process.env.NODE_ENV === 'production' ? '/enzo-bomark/' : '/',
   resolve: {
     alias: [
       { find: '~', replacement: path.resolve(__dirname, 'src') },
