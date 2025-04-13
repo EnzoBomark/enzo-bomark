a4333956047f41db59f3656c56dc9cf741e83581
Enzo Bomark <71819938+EnzoBomark@users.noreply.github.com>
2025-04-13 14:59:27 +0200

    chore: update package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vanillaExtractPlugin()],
+  base: '/enzo-bomark/',
   resolve: {
     alias: [
       { find: '~', replacement: path.resolve(__dirname, 'src') },
